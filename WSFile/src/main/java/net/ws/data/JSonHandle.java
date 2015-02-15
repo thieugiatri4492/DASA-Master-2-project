2f60f570d25bf9144b64e57af275306ed386fa42
thieugiatri4492 <hokage4492@gmail.com>
2015-02-15 21:21:28 +0700

    Json read write and get file,token fix

---
+++ WSFile/src/main/java/net/ws/data/JSonHandle.java
@@ -0,0 +1,60 @@
+package net.ws.data;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+
+
+public class JSonHandle {
+
+	
+	public JSonHandle(){
+		
+	}
+	
+	public void writeJson( String content,String name_PathFile){
+		File file = new File(name_PathFile);
+		System.out.println(file.getAbsolutePath());
+		try (FileOutputStream fop = new FileOutputStream(file)) {
+			 
+			// if file doesn't exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+ 
+			// get the content in bytes
+			byte[] contentInBytes = content.getBytes();
+ 
+			fop.write(contentInBytes);
+			fop.flush();
+			fop.close();
+ 
+			System.out.println("Done");
+ 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	@SuppressWarnings("unchecked")
+	public String readJson (String Filename){
+		 JSONParser parser = new JSONParser();
+		 String token=null;
+		 try {
+			  
+	            Object obj = parser.parse(new FileReader(
+	            		".\\src\\main\\webapp\\WEB-INF\\"+Filename));
+	            JSONObject jsonObject = (JSONObject) obj;
+	            token = (String) jsonObject.get("access_token");
+	 
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+		 return token;
+		 
+	}
+}
--- WSFile/src/main/java/net/ws/service/FileServlet.java
+++ WSFile/src/main/java/net/ws/service/FileServlet.java
@@ -1,18 +1,14 @@
 package net.ws.service;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.http.Header;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
@@ -21,37 +17,15 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
-import sun.net.www.protocol.http.HttpURLConnection;
-import net.ws.object.ClientFile;
-
-import com.google.api.client.http.ByteArrayContent;
-import com.google.api.client.http.GenericUrl;
-import com.google.api.client.http.HttpResponse;
-import com.google.api.services.drive.Drive;
-import com.google.api.services.drive.model.File;
+import net.ws.data.JSonHandle;
 
 public class FileServlet extends HandleCre {
+	 private HttpClient  client = new DefaultHttpClient();
+	 private JSonHandle json;
 	
-
-	private String downloadFileContent(Drive service, File file)
-			throws IOException {
-		GenericUrl url = new GenericUrl(file.getDownloadUrl());
-		HttpResponse response = service.getRequestFactory()
-				.buildGetRequest(url).execute();
-		try {
-			return new Scanner(response.getContent()).useDelimiter("\\A")
-					.next();
-		} catch (java.util.NoSuchElementException e) {
-			return "";
-		}
-	}
-	
-	@Override
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
-		//	resp.sendRedirect("https://www.googleapis.com/drive/v2/files?code="+req.getParameter("code"));
-		System.out.println(req.getParameter("code"));
-		HttpClient  client = new DefaultHttpClient();
+	 
+	public void exchangeToken (HttpServletRequest req) throws ClientProtocolException, IOException{
+		
         HttpPost post = new HttpPost("https://accounts.google.com/o/oauth2/token");
         List <NameValuePair>pairs = new ArrayList<NameValuePair>();
         pairs.add(new BasicNameValuePair("code", req.getParameter("code")));
@@ -62,39 +36,29 @@ public class FileServlet extends HandleCre {
         post.setEntity(new UrlEncodedFormEntity(pairs));
         org.apache.http.HttpResponse response = client.execute(post);
         String responseBody = EntityUtils.toString(response.getEntity());
-       /* System.out.println(responseBody);
-        System.out.println("response:-------------------");
-		System.out.println(response.getStatusLine());
-		Header[] headers = response.getAllHeaders();
-		for(Header h:headers){
-			System.out.println(h.getName() + ": " + h.getValue());
-		}*/
         
-        HttpGet request= new HttpGet("https://www.googleapis.com/drive/v2/files");
-        request.setHeader("Authorization", "Bearer "+"ya29.GQEXozQza5qWB3OfU-zNOdbLcplR_jcAiGZ86YtiDLv0F-yaqs1IRxEEoOUtP8oZyQy5WaZF-fLmmw");
+        this.json = new JSonHandle();
+        this.json.writeJson(responseBody, "src/main/webapp/WEB-INF/token.json");
+        
+        System.out.println(responseBody);
+	}
+	public void geFileList () throws ClientProtocolException, IOException{
+		
+		 this.json = new JSonHandle();
+	     String token =this.json.readJson("token.json");
+		HttpGet request= new HttpGet("https://www.googleapis.com/drive/v2/files");
+        request.setHeader("Authorization", "Bearer "+token);
         org.apache.http.HttpResponse respo=client.execute(request);
         String therespond = EntityUtils.toString(respo.getEntity());
+       this.json.writeJson(therespond, "src/main/webapp/WEB-INF/File-Info/filegoogle.json");
         System.out.println(therespond);
-        
-			 
 	}
-
+	
 	@Override
-	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		Drive service = getDriveService(getCredential(req, resp));
-		ClientFile clientFile = new ClientFile(req.getReader());
-		File file = clientFile.toFile();
-
-		if (!clientFile.content.equals("")) {
-			file = service
-					.files()
-					.insert(file,
-							ByteArrayContent.fromString(clientFile.mimeType,
-									clientFile.content)).execute();
-		} else {
-			file = service.files().insert(file).execute();
-		}
-		sendJson(resp, file.getId());
+        this.exchangeToken(req);
+        this.geFileList();
+			 
 	}
 }
